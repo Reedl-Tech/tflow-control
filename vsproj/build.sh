b7225ea98df5c3f60e955f5383ff4aacda9b3477
Andrejs Vasiljevs <av@reedl.tech>
2025-01-20 09:55:33 +0200

    added build script

--- vsproj/build.sh
+++ vsproj/build.sh
@@ -12,7 +12,7 @@ cd ${CURR_PATH}
 pwd
 
 echo "== 3 =="
-bitbake -c $1 tflow-process
+bitbake -c $1 tflow-control
 
 #echo "== 4 =="
 #cat /home/av/imx/build-ucm-imx8m-plus/tmp/work/cortexa53-crypto-poky-linux/tflow-process/1.0-r0/temp/log.do_compile >&2
